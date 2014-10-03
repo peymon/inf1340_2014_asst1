3d9fdafb2bef675057af5e9e1c4d8ec04415e46c
Peymon <peymon@gmail.com>
2014-10-03 15:44:54 -0400

    0% from scratch

--- exercise2.py
+++ exercise2.py
@@ -6,10 +6,10 @@
     Assignment 1, Exercise 2, INF1340 Fall 2014
 """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = 'Peymon & Haoran'
+__email__ = ""
 
-__copyright__ = "2014 Susan Sim"
+__copyright__ = "2014 PeyRan"
 __license__ = "MIT License"
 
 __status__ = "Prototype"
@@ -31,22 +31,18 @@ def checksum (upc):
     """
 
     # check type of input
-    if upc is not str:
     # raise TypeError if not string
-         raise TypeError("Wrong input, try again")
+
     # check length of string
-    elif len(upc) != 12:
     # raise ValueError if not 12
-         raise ValueError("Wrong input, try again")
+
     # convert string to array
     # hint: use the list function
-    else:
-        num = list(upc)
+
     # generate checksum using the first 11 digits provided
-    sum=3*(int(num(0))+int(num(2))+int(num(4))+int(num(6))+int(num(8))+int(num(10)))+int(num(1))+int(num(3))+int(num(5))+int(num(7))+int(num(9));
     # check against the the twelfth digit
-    if sum == int(num(11)):
+
     # return True if they are equal, False otherwise
-        return True
+
     return False
 
