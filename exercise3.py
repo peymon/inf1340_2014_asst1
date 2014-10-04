4df4f9fa4ed766761016110c8273cb5a169c2a62
Devin <wanghaoran1128@163.com>
2014-10-03 21:51:57 -0500

    ex3 95% done

--- exercise3.py
+++ exercise3.py
@@ -2,19 +2,23 @@
 
 
 def decide_rps(player1, player2):
-    dic = {"rock,paper":2, "rock,scissors":1, "paper,rock":1, "paper,scissors":2, "scissors,rock":2, "scissors,paper":1}
-    result = []
-    player1 = str(player1.lower())
-    player2 = str(player2.lower())
-    if player1 is not "paper" and not "rock" and not "scissors":
-        raise ValueError("Wrong input, try again")
-    elif player2 is not "paper" and not "rock" and not "scissors":
-        raise ValueError("Wrong input, try again")
-    elif player1 == player2:
-        return 0
-    else:
-        result.append(player1)
-        result.append(player2)
-        compare = ','.join(str(i) for i in result)
-        return dic[compare]
+    rps = {
+    ("Rock","Paper"):2,
+    ("Rock","Scissors"):1,
+    ("Rock","Rock"):0,
+    ("Paper","Scissors"):2,
+    ("Paper","Rock"):1,
+    ("Paper","Paper"):0,
+    ("Scissors","Rock"):2,
+    ("Scissors","Paper"):1,
+    ("Scissors","Scissors"):0
+    }
 
+    test=(player1,player2)
+
+    if type(player1) is not str or type(player2) is not str:
+        raise TypeError("Wrong input, try again")
+    elif test not in rps.keys():
+        raise ValueError("Wrong input, try again")
+    else:
+        return rps[test]
